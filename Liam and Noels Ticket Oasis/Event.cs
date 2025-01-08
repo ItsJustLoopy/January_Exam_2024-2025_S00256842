01c164ee83b07f92c50722b5d15213f6b4aee341
Sami Benaissa <S00256842@atu.ie>
2025-01-08 13:35:48 +0000

    Questions 1f,1g - Created Event class and implemented sort by EventDate using Icomparable

---
+++ Liam and Noels Ticket Oasis/Event.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liam_and_Noels_Ticket_Oasis
+{
+    internal class Event : IComparable<Event>
+    {
+        // Properties
+
+        public string Name { get; set; }
+        public DateTime EventDate { get; set; }
+        public List<Ticket> Tickets { get; set; }
+
+        public enum EventType
+        {
+            Music,
+            Comedy,
+            Theatre
+        }
+
+        //Constructors
+
+        //Methods
+
+        public int CompareTo(Event other)
+        {
+            if (other.EventDate > this.EventDate)
+            {
+                return -1;
+            }
+            else if (other.EventDate < this.EventDate)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
